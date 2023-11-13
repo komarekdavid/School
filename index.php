673317170f029af90c4c33e3e3d52e08131702ed
komarekdavid <david.komarek@student.ossp.cz>
2023-11-13 15:12:27 +0100

    Add PHP to the project

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="/assets/css/style.css">
+    <title>PHP - Include</title>
+</head>
+<body>
+    <?php include('components/header.html'); ?>
+
+    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quibusdam eos aliquid doloribus, suscipit quas ipsa illo amet natus sed deserunt aperiam vero. Ratione sint quo similique facilis aspernatur odio nulla?</p>
+
+    <?php include('components/footer.html'); ?>
+</body>
+</html>
